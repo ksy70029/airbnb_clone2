3bf5157dc1253f28b121459063ba89f96fb8b4dc
DGksy70029 <ksy70029@naver.com>
2022-05-18 15:41:44 +0900

    custom users admin

--- airbnb_clone/settings.py
+++ airbnb_clone/settings.py
@@ -29,21 +29,18 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'conversations',
-    'lists',
-    'reservations',
-    'reviews',
-    'rooms',
-    'users',
 ]
+PROJECT_APPS =[
+    'users.apps.UsersConfig',
+]
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -125,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#
+AUTH_USER_MODEL = 'users.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
