f70cd1dc074b0dd98beab053873e2499fd9c6385
DGksy70029 <ksy70029@naver.com>
2022-05-21 12:25:30 +0900

    Room ADMIN

--- rooms/admin.py
+++ rooms/admin.py
@@ -14,7 +14,66 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        (
+            "Times",
+            {"fields": ("check_in", "check_out", "instant_book")},
+        ),
+        (
+            "More About the Space",
+            {"classes": ('collapse',), "fields": ("amenities", "facilities", "house_rules")},
+        ),
+        (
+            "Spaces",
+            {"fields": ("guests", "beds", "bedrooms", "baths")},
+        ),
+        (
+            "List Details",
+            {"fields": ("host",)},
+        ),
+    )
+
+    ordering = ("name", "price", "bedrooms")
+
+    list_display = (
+        "name",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+    )
+
+    list_filter = (
+        "instant_book",
+        "room_type",
+        "host__gender",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_fields = ("city", "host__username")
+
+    filter_horizontal = ("amenities", "facilities", "house_rules",)
+
+    def count_amenities(self, obj):
+        print(obj.amenities.all())
+        return "totato"
+
+    count_amenities.short_description  = "hello sexy!"
+
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
